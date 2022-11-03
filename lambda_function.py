a6e29593ed1043234323d8c9cc00ccb494c2e89f
shahnawaz.rajper-at-184350108357 <rajadayo1@gmail.com>
2022-11-04 00:55:06 +0500

    update

--- lambda_function.py
+++ lambda_function.py
@@ -2,4 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    return 'Hi Raja..This is my first CICD practice example'
+      return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!....... (Modified v0.2')
+    }
